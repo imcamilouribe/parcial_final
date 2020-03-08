7c99ead90c90b183fd15c21278b67fd5838000b7
imcamilouribe <61896434+imcamilouribe@users.noreply.github.com>
2020-03-08 15:01:05 -0500

    Update web.py

--- local/web.py
+++ local/web.py
@@ -1,16 +1,11 @@
 from flask import Flask, request, render_template, jsonify, url_for, session, g, redirect
 import redis, json
-#the getparams function will return a datetime object with these parameters '%Y-%m-%d_%H:%M:%S' followed by temperature in the form of a float
 from params import getparams, justtime
 from flask_redis import FlaskRedis
 from flask_mysqldb import MySQL
 from flask_mqtt import Mqtt
 
 
-
-
-#weather api http://api.openweathermap.org/data/2.5/weather?id=3687238&appid=9583c3b4fa60a5323f4d1d115a5f2592
-#Flask configs
 app = Flask(__name__)
 mqtt = Mqtt()
 
@@ -33,7 +28,6 @@ app.config['MYSQL_DB'] = 'flaskapp'
 mysql = MySQL(app)
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-#"Gets the temperature and timestamp of the IOTD in the form of a json"
 def jsoniot():
     y= getparams()
     t = justtime()
@@ -41,7 +35,6 @@ def jsoniot():
     j = json.loads((x))
     return (j)
 
-#Adapter to make the json data into variables
 def readjson(j, human):
     temp = j["temperature"]
     times = j["timestamp"]
@@ -49,19 +42,15 @@ def readjson(j, human):
         addstuffdb(temp, times, human)
     return(temp, times)
 
-#Here's where the service adds the data to both databases
 def addstuffdb(temp, times , human):
     cur = mysql.connection.cursor()
 
     if human == 1:
 
 
-        #Adding params to redis
         r.rpush("temperatures",str(temp))
         r.rpush("timestamps",str(times))
         r.rpush("humantime",str(times))
-        #r.lrange("list",-1,-1) Prints the last object of the list
-        #adding params to mysql
         try:
             time = str(justtime())
             temp = str(getparams())
@@ -77,11 +66,8 @@ def addstuffdb(temp, times , human):
             mysql.connection.commit()
             cur.close()
     else:
-        #Adding params to redis
         r.rpush("temperatures",str(temp))
         r.rpush("timestamps",str(times))
-        #r.lrange("list",-1,-1) Prints the last object of the list
-        #adding params to mysql
         try:
             time = str(justtime())
             temp = str(getparams())
@@ -95,12 +81,10 @@ def addstuffdb(temp, times , human):
             cur.execute("INSERT INTO iot(time, temp) VALUES(%s, %s)",(time, temp))
             mysql.connection.commit()
             cur.close()
-#routes
+            
 @app.route('/index')
 def index():
     return (render_template('index.html'))
-
-
 
 @app.route("/=<var>", methods=['GET', 'POST'])
 def asdf(var):
@@ -109,8 +93,6 @@ def asdf(var):
         temp, times =readjson(j,0)
         return(render_template('iot.html', data = temp ))
 
-
-#This route will show every object added to the db
 @app.route("/", methods=['GET', 'POST'])
 def writehash():
 
@@ -122,8 +104,6 @@ def writehash():
         j = jsoniot()
         temp, times = readjson(j,1)
         return render_template('get.html', title= "Show temps", data = temp)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 8000)
